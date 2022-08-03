3bd9f9e5775754b71c5a4139bfe44a04a0995fae
Shane <navysupra@gmail.com>
2022-08-03 03:28:27 +0000

    managed to refactor out a couple lines, but didn't acheive 10

--- spinner2.js
+++ spinner2.js
@@ -1,25 +1,18 @@
 // minimum 1000ms duration
-const spinner = function() {
+const spinner = function(revs) {
   const spinners = [ `|`, `/`, `-`, `\\` ];
-  const delay = Math.round(1000 / 8);
   let spinning = true;
   let pos = 0;
-  const revs = 8;
 
   while (spinning) {
     const p = pos % 4;
-    const d = delay * pos;
+    const d = Math.round(1000 / 8) * pos;
     setTimeout(() => {
-      process.stdout.write(`\r${spinners[p]}            `);
+      const output = pos === revs ? `\r${spinners[p]}      ` : `\r${spinners[p]}\n`;
+      process.stdout.write(output);
     }, d);
-    if (pos === revs) {
-      setTimeout(() => {
-        console.log();
-      }, d);
-      spinning = false;
-    }
     pos++;
   }
 };
 
-spinner();
+spinner(8);
