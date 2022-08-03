63cfd407e74505dc6f3e290fed6d89a969f1b455
Shane <navysupra@gmail.com>
2022-08-03 03:00:42 +0000

    impelment the spinner!

--- spinner1.js
+++ spinner1.js
@@ -1,12 +1,25 @@
-setTimeout(() => {
-  process.stdout.write(`\r|   `);
-}, 100);
-setTimeout(() => {
-  process.stdout.write(`\r/   `);
-}, 300);
-setTimeout(() => {
-  process.stdout.write(`\r-   `);
-}, 500);
-setTimeout(() => {
-  process.stdout.write(`\r\\   `);
-}, 700);
+// minimum 1000ms duration
+const spinner = function() {
+  const spinners = [ `|`, `/`, `-`, `\\` ];
+  const delay = Math.round(1000 / 8);
+  let spinning = true;
+  let pos = 0;
+  const revs = 8;
+
+  while (spinning) {
+    const p = pos % 4;
+    const d = delay * pos;
+    setTimeout(() => {
+      process.stdout.write(`\r${spinners[p]}            `);
+    }, d);
+    if (pos === revs) {
+      setTimeout(() => {
+        console.log();
+      }, d);
+      spinning = false;
+    }
+    pos++;
+  }
+};
+
+spinner();
